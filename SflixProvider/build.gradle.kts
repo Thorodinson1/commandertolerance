ccb04bceec7794fa5e080384608cff4d4b69ac6c
Thorodinson1 <maniwertyu123@gmail.com>
2023-08-02 16:19:18 +0530

    init

---
+++ SflixProvider/build.gradle.kts
@@ -0,0 +1,22 @@
+// use an integer for version numbers
+version = 3
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+     description = "Also includes Aniwatch"
+    // authors = listOf("Cloudburst")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    
+    iconUrl = "https://www.google.com/s2/favicons?domain=sflix.to&sz=24"
+}
