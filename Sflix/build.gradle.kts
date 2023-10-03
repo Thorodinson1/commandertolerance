a1cd7d4a8dbbb1e368922095141738255123b750
Thorodinson1 <maniwertyu123@gmail.com>
2023-10-03 18:49:44 +0530

    uuuuu

---
+++ Sflix/build.gradle.kts
@@ -0,0 +1,22 @@
+// use an integer for version numbers
+version = 16
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+     description = "Movies"
+    // authors = listOf("Cloudburst")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    
+    iconUrl = "https://www.google.com/s2/favicons?domain=sflix.to&sz=24"
+}
---
+++ Sflix/src/main/kotlin/com/commandertolerance/SflixProviderPlugin.kt
@@ -0,0 +1,13 @@
+package com.commandertolerance
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SflixProviderPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(SflixProvider())
+    }
+}
