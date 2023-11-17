770373171ef145b304096c5857d82a94e97cd20d
Thorodinson1 <maniwertyu123@gmail.com>
2023-11-17 22:27:46 +0530

    decl

--- Mlfnut/src/main/kotlin/com/hexated/MlfnutPlugin.kt
+++ Mlfnut/src/main/kotlin/com/hexated/MlfnutPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Mlfnut: Plugin() {
+class MlfnutPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         //registerExtractorAPI(StreamTapeTo())
