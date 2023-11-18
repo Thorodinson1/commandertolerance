223b5cafac473068805d4b858cafa700ee52f8c0
Thorodinson1 <maniwertyu123@gmail.com>
2023-11-18 09:32:59 +0530

    fix

---
+++ Aniwatch/build.gradle.kts
@@ -0,0 +1,22 @@
+// use an integer for version numbers
+version = 19
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+     description = "Aniwatch Anime"
+    // authors = listOf("Cloudburst")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    
+    iconUrl = "https://www.google.com/s2/favicons?domain=aniwatch.to&sz=24"
+}
