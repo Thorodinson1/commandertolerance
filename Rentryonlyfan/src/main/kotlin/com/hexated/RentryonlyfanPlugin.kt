c9d92da098350a96c36225a5194db3459724a869
Thorodinson1 <maniwertyu123@gmail.com>
2024-01-30 23:05:59 +0530

    ee

--- Rentryonlyfan/src/main/kotlin/com/hexated/RentryonlyfanPlugin.kt
+++ Rentryonlyfan/src/main/kotlin/com/hexated/RentryonlyfanPlugin.kt
@@ -8,7 +8,7 @@ import android.content.Context
 class RentryonlyfanPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerExtractorAPI(Dooood())
+        //registerExtractorAPI(Dooood())
         registerMainAPI(Rentryonlyfan())
     }
 }
