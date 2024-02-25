b3219dfcddf67c3248f81ccf5aca662b4656d731
Thorodinson1 <maniwertyu123@gmail.com>
2024-02-25 06:43:27 +0530

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,11 +78,11 @@ subprojects {
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.4.4") // http library
-        implementation("org.jsoup:jsoup:1.16.2") // html parser
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
+        implementation("org.jsoup:jsoup:1.17.2") // html parser
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
         implementation("io.karn:khttp-android:0.1.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // html parser
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0") // html parser
         implementation("org.mozilla:rhino:1.7.14") //run JS
 
     }
